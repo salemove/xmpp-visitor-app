10bd7f0379286ec49e65dd5a842ec65ca0302cc9
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2017-09-30 03:49:27 +0300

    Register and unregister visitors in Jabber

--- api.rb
+++ api.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'rom-repository'
+require 'xmpp4r'
 
 JABBER_DOMAIN = '10.200.0.138'.freeze
+API_JID = "api@#{JABBER_DOMAIN}".freeze
 
 rom = ROM.container(:sql, 'postgres://localhost/tcobr') do |conf|
 end
@@ -16,9 +18,13 @@ class Visitor
   def serialize
     {
       id: @id,
-      jid: "#{@id}@#{JABBER_DOMAIN}",
+      jid: jid,
       password: @password
     }
+  end
+
+  def jid
+    "#{@id}@#{JABBER_DOMAIN}"
   end
 end
 
@@ -29,12 +35,16 @@ class VisitorRepo < ROM::Repository[:visitors]
     visitors.as(Visitor).to_a
   end
 
+  def get(id)
+    visitors.as(Visitor).where(id: id).one!
+  end
+
   def delete(id)
-    visitors.where(id: id).delete
+    visitors.as(Visitor).where(id: id).delete
   end
 
   def delete_all
-    visitors.delete
+    visitors.as(Visitor).delete
   end
 end
 visitor_repo = VisitorRepo.new(rom)
@@ -44,13 +54,39 @@ get '/visitors' do
 end
 
 post '/visitors' do
-  visitor_repo.create(password: params[:password]).to_h
+  v = visitor_repo.create(password: params.fetch(:password))
+  register(v.fetch(:id), params.fetch(:password))
 end
 
 delete '/visitors/:id' do |id|
+  visitor = visitor_repo.get(id)
+  unregister(visitor)
   visitor_repo.delete(id)
 end
 
 delete '/visitors' do
+  visitor_repo.all.each(&method(:unregister))
   visitor_repo.delete_all
+end
+
+def register(id, password)
+  jid = "#{id}@#{JABBER_DOMAIN}"
+  cl = Jabber::Client.new(Jabber::JID.new(jid))
+  cl.connect
+  begin
+    cl.register(password)
+  ensure
+    cl.close
+  end
+end
+
+def unregister(visitor)
+  cl = Jabber::Client.new(Jabber::JID.new(visitor.jid))
+  cl.connect
+  begin
+    cl.auth(visitor.password)
+    cl.remove_registration
+  ensure
+    cl.close
+  end
 end
